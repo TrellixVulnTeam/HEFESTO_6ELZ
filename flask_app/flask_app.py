254b48b2a30694d16e7112b7de329161ad0dbffe
LuisAndraschko <luis_andraschko@hotmail.com>
2022-08-25 21:53:01 -0300

    Login and Register pages

--- flask_app/flask_app.py
+++ flask_app/flask_app.py
@@ -1,12 +1,16 @@
+# from crypt import methods
 from turtle import title
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LogInForm
 
 app = Flask(__name__)
 
-titles = ['Pu Conversion Tool']
+app.config['SECRET_KEY'] = '47665f0caf395f8b7cfb54faac032245'
+
+titles = ['Pu Conversion Tool', 'Register', 'Login']
 
 dummy_input = [
-    {
+    {  
         'description': 'System base values',
         'power': '100MVA',
         'voltage': '13.8kV in Bar 1'
@@ -17,6 +21,29 @@ dummy_input = [
 @app.route("/home")
 def home():
     return render_template('home.html')
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Conta criada para {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template("register.html", title=titles[1], form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LogInForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@gmail.com' and form.password.data == '12345678':
+            flash(f'Seja bem vindo {form.email.data}!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(f"Sessão não pode ser iniciada, forneça e-mail e senha adequados!", 'danger')
+    return render_template('login.html', title=titles[2], form=form)
 
 @app.route("/puconversions")
 def pu_conversion():
--- flask_app/forms.py
+++ flask_app/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, InputRequired
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', 
-                            validators=[DataRequired(), Length(min=8, max=20)])
+    username = StringField('Nome', 
+                            validators=[InputRequired('saduajbda'), Length(min=8, max=20, message='asdsafdsfd')])
     email = StringField('E-mail', 
                         validators=[DataRequired(), Email()])
     password = PasswordField('Senha', 
@@ -21,8 +21,4 @@ class LogInForm(FlaskForm):
     password = PasswordField('Senha', 
                             validators=[DataRequired(), Length(min=8)])
     remember = BooleanField('Lembrar-me')
-    submit = SubmitField('Entrar')
-
-
-class PuConversionForm(FlaskForm):
-    
+    submit = SubmitField('Iniciar Sessão')
