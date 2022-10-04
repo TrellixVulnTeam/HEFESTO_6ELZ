95eb6860d8011fd46ccd104c5e631ae6d3609076
LuisAndraschko <luis_andraschko@hotmail.com>
2022-10-04 08:33:24 -0300

    Note novo config

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Estudos\TCC\HEFESTO\venv
+set VIRTUAL_ENV=C:\Users\Luís Andraschko\Documents\HEFESTO\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
